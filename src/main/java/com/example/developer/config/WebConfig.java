67089ecdc3a217b46701c15ace8cf7f311533eda
Abhinav Jain <abhinavjain141@gmail.com>
2024-10-23 23:00:25 +0530

    COMMIT -16

--- src/main/java/com/example/developer/config/WebConfig.java
+++ src/main/java/com/example/developer/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow all endpoints
-                .allowedOrigins("http://localhost:3001") // Change to your frontend URL
+                .allowedOrigins("http://localhost:3000") // Change to your frontend URL
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
     }
 }
